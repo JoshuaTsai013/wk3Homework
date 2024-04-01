6105d8c942853d3bea7302d456c7ed0030e9cc6f
joshuatsai <joshuatsai1213@gmail.com>
2024-04-02 00:15:03 +0800

    better looks

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text } from "@gluestack-ui/themed";
+import { Text, Center } from "@gluestack-ui/themed";
 
 const AccountScreen = () => {
     return (
-    <View style={{flex: 1}}>
-        <Text fontSize={30}>
-            This is a AccountScreen
-        </Text>              
-    </View>
+        <View style={{ flex: 1 }}>
+            <Center
+                shadow={2} width="90%"
+                mt="$2" px="$2" py="$4"
+                bg="white" borderRadius={3}
+                alignSelf="center">
+                <Text fontSize={30}>
+                    This is AccountScreen
+                </Text>
+            </Center>
+        </View>
     );
 }
 
--- src/screens/myBookScreen.js
+++ src/screens/myBookScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text } from "@gluestack-ui/themed";
+import { Text, Center } from "@gluestack-ui/themed";
 
 const MyBookScreen = () => {
     return (
-    <View style={{flex: 1}}>
-        <Text fontSize={30}>
-            This is a MyBookScreen
-        </Text>              
-    </View>
+        <View style={{ flex: 1 }}>
+            <Center
+                shadow={2} width="90%"
+                mt="$2" px="$2" py="$4"
+                bg="white" borderRadius={3}
+                alignSelf="center">
+                <Text fontSize={30}>
+                    This is MyBookScreen
+                </Text>
+            </Center>
+        </View>
     );
 }
 
